a4bdbdc7c35242122ff54c5bdb7ae6b54efef647
Michael J. Bennett <dev@michaeljbennett.info>
2019-12-15 14:39:31 -0500

    Simple pixel plotter in webgl

--- index2.ts
+++ index2.ts
@@ -32,8 +32,9 @@ const fragmentSource = `precision mediump float;
     varying vec2 v_texcoord;
      
     void main() {
-       float xNorm = gl_FragCoord.x / 640.0;
-       float yNorm = gl_FragCoord.y / 480.0;
+       float xNorm = (gl_FragCoord.x / 255.0 / 640.0) * 255.0;
+       float yNorm = (gl_FragCoord.y / 255.0 / 480.0) * 255.0;
+       
        gl_FragColor = vec4(xNorm, yNorm, 0.8, 1.0);
     }
 `;
@@ -133,3 +134,204 @@ function draw(gl: WebGLRenderingContext, context: ProgramContext) {
     gl.drawArrays(gl.TRIANGLES, 0, 6);
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+const scene = {
+    camera: {
+        point: {
+            x: 0,
+            y: 1.8,
+            z: 10
+        },
+        fieldOfView: 45,
+        vector: {
+            x: 0,
+            y: 3,
+            z: 0
+        }
+    },
+    lights: [{
+        x: -30,
+        y: -10,
+        z: 20
+    }],
+    objects: [
+        {
+            type: 'sphere',
+            point: {
+                x: 0,
+                y: 3.5,
+                z: -3
+            },
+            color: {
+                x: 155,
+                y: 200,
+                z: 155
+            },
+            specular: 0.2,
+            lambert: 0.7,
+            ambient: 0.1,
+            radius: 3
+        },
+        {
+            type: 'sphere',
+            point: {
+                x: -4,
+                y: 2,
+                z: -1
+            },
+            color: {
+                x: 155,
+                y: 155,
+                z: 155
+            },
+            specular: 0.1,
+            lambert: 0.9,
+            ambient: 0.0,
+            radius: 0.2
+        },
+        {
+            type: 'sphere',
+            point: {
+                x: -4,
+                y: 3,
+                z: -1
+            },
+            color: {
+                x: 255,
+                y: 255,
+                z: 255
+            },
+            specular: 0.2,
+            lambert: 0.7,
+            ambient: 0.1,
+            radius: 0.1
+        }
+    ],
+};
+
+// function render(scene) {
+//     // first 'unpack' the scene to make it easier to reference
+//     var camera = scene.camera,
+//         objects = scene.objects,
+//         lights = scene.lights;
+
+//     // This process
+//     // is a bit odd, because there's a disconnect between pixels and vectors:
+//     // given the left and right, top and bottom rays, the rays we shoot are just
+//     // interpolated between them in little increments.
+//     //
+//     // Starting with the height and width of the scene, the camera's place,
+//     // direction, and field of view, we calculate factors that create
+//     // `width*height` vectors for each ray
+
+//     // Start by creating a simple vector pointing in the direction the camera is
+//     // pointing - a unit vector
+//     var eyeVector = Vector.unitVector(Vector.subtract(camera.vector, camera.point)),
+
+//         // and then we'll rotate this by combining it with a version that's turned
+//         // 90° right and one that's turned 90° up. Since the [cross product](http://en.wikipedia.org/wiki/Cross_product)
+//         // takes two vectors and creates a third that's perpendicular to both,
+//         // we use a pure 'UP' vector to turn the camera right, and that 'right'
+//         // vector to turn the camera up.
+//         vpRight = Vector.unitVector(Vector.crossProduct(eyeVector, Vector.UP)),
+//         vpUp = Vector.unitVector(Vector.crossProduct(vpRight, eyeVector)),
+
+//         // The actual ending pixel dimensions of the image aren't important here -
+//         // note that `width` and `height` are in pixels, but the numbers we compute
+//         // here are just based on the ratio between them, `height/width`, and the
+//         // `fieldOfView` of the camera.
+//         fovRadians = Math.PI * (camera.fieldOfView / 2) / 180,
+//         heightWidthRatio = height / width,
+//         halfWidth = Math.tan(fovRadians),
+//         halfHeight = heightWidthRatio * halfWidth,
+//         camerawidth = halfWidth * 2,
+//         cameraheight = halfHeight * 2,
+//         pixelWidth = camerawidth / (width - 1),
+//         pixelHeight = cameraheight / (height - 1);
+
+//     var index, color;
+//     var ray = {
+//         point: camera.point
+//     };
+//     for (var x = 0; x < width; x++) {
+//         for (var y = 0; y < height; y++) {
+
+//             // turn the raw pixel `x` and `y` values into values from -1 to 1
+//             // and use these values to scale the facing-right and facing-up
+//             // vectors so that we generate versions of the `eyeVector` that are
+//             // skewed in each necessary direction.
+//             var xcomp = Vector.scale(vpRight, (x * pixelWidth) - halfWidth),
+//                 ycomp = Vector.scale(vpUp, (y * pixelHeight) - halfHeight);
+
+//             ray.vector = Vector.unitVector(Vector.add3(eyeVector, xcomp, ycomp));
+
+//             // use the vector generated to raytrace the scene, returning a color
+//             // as a `{x, y, z}` vector of RGB values
+//             color = trace(ray, scene, 0);
+//             index = (x * 4) + (y * width * 4),
+//             data.data[index + 0] = color.x;
+//             data.data[index + 1] = color.y;
+//             data.data[index + 2] = color.z;
+//             data.data[index + 3] = 255;
+//         }
+//     }
+
+//     // Now that each ray has returned and populated the `data` array with
+//     // correctly lit colors, fill the canvas with the generated data.
+//     ctx.putImageData(data, 0, 0);
+// }
